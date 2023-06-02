a44c12e15a2beeddf7e94f3630fa0b7740bb9519
Nathalia Dias <nathalia.dias3010@gmail.com>
2023-06-01 21:15:49 -0300

    Adicionando Api Inicial

---
+++ PosgramAPI/PosgramAPI/Models/Dto/PostagemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PosgramAPI.Models.Dto
+{
+    public class PostagemDto
+    {
+        public int Id { get; set; }
+        public string Autor { get; set; }
+        public string Legenda { get; set; }
+        public string Imagem { get; set; }
+        public DateTime? DataHoraPostagem { get; set; }
+    }
+}
