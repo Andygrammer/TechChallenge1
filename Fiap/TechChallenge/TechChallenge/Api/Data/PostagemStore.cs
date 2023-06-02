189fe9e45a657eec5677323fccf519ca24512d10
Rodrigo Castagnaro <rodrigoskte@hotmail.com>
2023-06-02 20:33:32 -0300

    master

---
+++ Fiap/TechChallenge/TechChallenge/Api/Data/PostagemStore.cs
@@ -0,0 +1,16 @@
+﻿using PosgramAPI.Models.Dto;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PosgramAPI.Data
+{
+    public class PostagemStore
+    {
+        public static List<PostagemDto> listaPostagem = new List<PostagemDto>
+        {
+            new PostagemDto {Id=1, Autor="Caio", Legenda="Lorem ipsum dolor sit amet, consectetur adipiscing elit.", Imagem="https://picsum.photos/200/300", DataHoraPostagem=DateTime.Now },
+             new PostagemDto {Id=2, Autor="Maria", Legenda="Lorem ipsum dolor sit amet, consectetur adipiscing elit.", Imagem="https://picsum.photos/200/300", DataHoraPostagem=DateTime.Now},
+              new PostagemDto {Id=3, Autor="Thiago", Legenda="Lorem ipsum dolor sit amet, consectetur adipiscing elit.", Imagem="https://picsum.photos/200/300", DataHoraPostagem=DateTime.Now }
+        };
+
+    }
+}
---
+++ Fiap/TechChallenge/TechChallenge/WebBlazor/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+using PosgramWasm;
+
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+
+await builder.Build().RunAsync();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
