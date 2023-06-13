6dbc1b56b0cd6bb29d50fd4512bb0c6edde5b322
DESKTOP-4VSD9TI\andre <andremarinho21@outlook.com>
2023-06-13 19:16:17 -0300

    fix-4: configuração do swagger para uso no Azure

--- Api/Program.cs
+++ Api/Program.cs
@@ -24,7 +24,8 @@ app.UseSwagger();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwaggerUI();
+    //app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PosgramWasm v1"));
 }
 
 app.UseHttpsRedirection();
