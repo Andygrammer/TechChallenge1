6d03c2170749a741e8d535d51f1b0a13977dc68b
Vinícius Falcão <vinicius.falcao@lambda3.com.br>
2023-06-14 16:59:53 -0300

    criacao de servico para CRUD no blob

---
+++ Api/Services/BlobServices.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Storage.Blobs;
+using Infraestrutura.Models;
+
+namespace Api.Services
+{
+    public interface IBlobServices
+    {
+        public Task<string> GetBlob(string imageName);
+        public Task CreateBlob(Postagem newPost);
+        public Task DeleteBlob(string imageName);
+    }
+
+    public class BlobServices : IBlobServices
+    {
+        private BlobContainerClient containerClient;
+        private readonly IConfiguration _configuration;
+
+        public BlobServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var azStorageConn = configuration.GetValue<string>("AzureConnectionStrings:StorageAccountConnection");
+            var blobServiceClient = new BlobServiceClient(azStorageConn);
+            containerClient = blobServiceClient.GetBlobContainerClient("sc-postechchallenge1");
+        }
+
+        public async Task CreateBlob(Postagem newPost)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(BuildBlobImagem(newPost));
+            var binaryData = Base64Service.ConvertFromBase64(newPost.Imagem);
+            await blobClient.UploadAsync(binaryData, true);
+        }
+
+        public async Task DeleteBlob(string imageName)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(imageName);
+            await blobClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<string> GetBlob(string imageName)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(imageName);
+            var memoStream = new MemoryStream();
+            await blobClient.DownloadToAsync(memoStream);
+            var base64String = Base64Service.ConvertToBase64(memoStream);
+            memoStream.Close();
+            return base64String;
+        }
+            
+        private string BuildBlobImagem(Postagem newPost)
+        {
+            var extensionName = Path.GetExtension(newPost.NomeImagem);
+            var nomeAutor = newPost.Autor.ToLower().Replace(" ", "-");
+            return string.Format("{0}_{1}{2}", nomeAutor, Guid.NewGuid().ToString(), extensionName);
+        }
+    }
+}
--- Infraestrutura/Models/Modelo.cs
+++ Infraestrutura/Models/Modelo.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infraestrutura.Models
 {
@@ -24,6 +19,8 @@ namespace Infraestrutura.Models
         [Required]
         public string Imagem { get; set; }
         [Required]
+        public string NomeImagem { get; set; }
+        [Required]
         public DateTime DataHora { get; set; }
 
         /// <summary>
@@ -36,12 +33,14 @@ namespace Infraestrutura.Models
         public Postagem(string autor, 
                         string legenda, 
                         string imagem, 
+                        string nomeImagem,
                         DateTime dataHora)
         {
             Autor = autor;
             Legenda = legenda;
             Imagem = imagem;
             DataHora = dataHora;
+            NomeImagem = nomeImagem;
         }
     }
 }
